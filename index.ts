521d6d02cab04eca11826d3cca4c1f581a36434d
AroSaneD <mail.junk.3135@gmail.com>
2024-06-24 19:46:48 +0300

    Added typescript, started writing new structure

---
+++ index.ts
@@ -0,0 +1,13 @@
+import * as robotjs from "robotjs";
+
+// function rightClick() {
+//   robotjs.mouseClick("right");
+// }
+
+// rightClick();
+function parseParams(args: string[]) {
+  // todo: implement
+  throw Error("Not implemented");
+}
+
+const params = parseParams(process.argv);
